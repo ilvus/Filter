443bf02483345e83bc4f73afc8151805f1733687
Azamat <101277772+ilvus@users.noreply.github.com>
2022-05-15 00:42:15 +0600

    Add files via upload

---
+++ pillow.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+
+class Picture:
+
+    def __init__(self, photo):
+        self.photo = Image.open(photo)
+
+    def crop_method(self):
+        area = (100, 100, 1180, 1180)
+        self.photo = self.photo.crop(area)
+
+    def bw(self):
+        self.photo = self.photo.convert("L")
+
+    def display(self):
+        self.photo.show()
+
+
+Photos = Picture("E:\Study\Pillow\img\dog.jpeg")
+Photos.crop_method()
+Photos.bw()
+Photos.display()
